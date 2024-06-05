e373f00c8e4d1409e6216ced0fa4e157f9cb7375
Henry Kim <127216529+thethirdbirthday@users.noreply.github.com>
2024-06-05 18:52:01 -0400

    Update c550_0.py

--- c550_0.py
+++ c550_0.py
@@ -4,3 +4,15 @@ name = input("What's your name?").strip().title()
 
 #Say hello to user 
 print(f"hello, {name}")
+
+# Values and Functions original
+name = input("what's your name?")
+
+# Remove space from str
+name = name.strip()
+
+# Capitalize user's name
+name = name.capitalize()
+
+#say hello to user
+print(f"hello, {name}")
