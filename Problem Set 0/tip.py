ce769960bcb264290366b2f78af8ed11596be793
Henry Kim <127216529+thethirdbirthday@users.noreply.github.com>
2024-06-11 16:33:45 -0400

    Create tip.py

---
+++ Problem Set 0/tip.py
@@ -0,0 +1,20 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+  subtotal = dollars.removeprefix("$")
+  return float(subtotal)
+    # TODO
+  
+
+def percent_to_float(p):
+  decimal = percent.removeprefix("%")
+  return float(decimal)/100
+    # TODO
+
+
+main()
