e9490aa5d097e8a5920f5a151164f44a76d843af
Henry Kim <127216529+thethirdbirthday@users.noreply.github.com>
2024-06-24 16:15:52 -0400

    Update deep.py

--- Problem Set 1/deep.py
+++ Problem Set 1/deep.py
@@ -8,7 +8,7 @@ if m = forty two
   print ("yes")
 elif print ("no")
 
-#take 2: make yes inputs shorter, "==" for equals instead of assinging value to variable
+#take 2: make yes inputs shorter, "==" for equals instead of assigning values to variables
 m = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
 if m == "forty-two" or m == "42" or m == "forty two" 
   print ("yes")
