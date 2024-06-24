b63912f673f106762f62accae40c1f90a9e906ed
Henry Kim <127216529+thethirdbirthday@users.noreply.github.com>
2024-06-24 16:22:42 -0400

    Create bank.py

---
+++ Problem Set 1/bank.py
@@ -0,0 +1,11 @@
+#ask for greeting
+m = input("Hello!" )
+#if greeting starts with h, return 20 bucks
+if m = "Hello" 
+  print("$0")
+#if greeting is hello return nothing
+elif m.startswith("H") 
+  print("$20")
+#lse, return 100 bucks
+else:
+  print("$100)
