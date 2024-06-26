dbe707177cb3c6593956b1558f5dec77fdaae090
Henry Kim <127216529+thethirdbirthday@users.noreply.github.com>
2024-06-26 18:32:05 -0400

    Update bank.py

--- Problem Set 1/bank.py
+++ Problem Set 1/bank.py
@@ -1,16 +1,26 @@
 #ask for greeting
-m = input("Hello!" )
+#m = input("Hello!" )
 #account for case insensitive answers
-i = m.lower()
+#i = m.lower()
 #if greeting starts with h, return 20 bucks
-if i = "hello" 
-  print("$0")
+#if i = "hello" 
+ # print("$0")
 #if greeting is hello return nothing
-elif m.startswith("h") 
-  print("$20")
+#elif m.startswith("h") 
+ # print("$20")
 #else, return 100 bucks
-else:
-  print("$100)
+#else:
+ # print("$100)
 
 #alternative for checking for h answer
-        elif h == i[0] 
+#elif h == i[0] 
+
+#take 2
+user_input = input().lower()
+if user_input.startswith("hello"):
+  print("$0")
+elif user_input.startswith("h"): 
+  print("$20")
+else:
+  print("$100")
+
