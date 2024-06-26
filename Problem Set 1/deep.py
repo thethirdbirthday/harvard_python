43f75c8de05d359b7bab67283515d0fe402f58ca
Henry Kim <127216529+thethirdbirthday@users.noreply.github.com>
2024-06-26 18:34:30 -0400

    Update deep.py

--- Problem Set 1/deep.py
+++ Problem Set 1/deep.py
@@ -1,16 +1,16 @@
 #take 1
-m = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
-if m = "forty-two"
-  print ("yes")
-if m = 42 
-  print ("yes")
-if m = forty two 
-  print ("yes")
-elif print ("no")
+#m = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+#if m = "forty-two"
+ # print ("yes")
+#if m = 42 
+ # print ("yes")
+#if m = forty two 
+ # print ("yes")
+#elif print ("no")
 
 #take 2: make yes inputs shorter, "==" for equals instead of assigning values to variables
 m = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
-if m == "forty-two" or m == "42" or m == "forty two" 
+if m == "forty-two" or m == "42" or m == "forty two":
   print ("yes")
 else:
   print("no")
