dc8fe49fa8eb98743032ccc0beb7614cd93470d4
Henry Kim <127216529+thethirdbirthday@users.noreply.github.com>
2024-07-01 14:39:15 -0400

    Update extensions.py

--- Problem Set 1/extensions.py
+++ Problem Set 1/extensions.py
@@ -1,1 +1,20 @@
-input("What is the name of the file?" )
+#ask for file name
+#Remove spaces and make input lowercase
+#replace file extension with MIME type
+#image application text octet
+
+file = input("What is the file name? ")
+true_file = file.lower()
+if 'gif' in true_file:
+    print("image.gif")
+elif 'jpg' or 'jpeg' in true_file:
+    print(image/jpeg)
+elif 'png' in true_file:
+    print("image/png")
+elif 'pdf' in true_file:
+    print("application/pdf")
+elif 'txt' in true_file:
+    print("txt/plain")
+elif 'zip' in true_file:
+    print("application/zip")
+else print("application/octet-stream")
